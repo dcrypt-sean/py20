2266eed07134d48251443cf69d1c20ea4776cd72
dcrypt-sean <sean@dcrypt.us>
2018-08-30 11:30:18 -0400

    added colors

--- py20.py
+++ py20.py
@@ -1,9 +1,15 @@
 import random as r
 
+class color:
+    MAIN = '\033[32m'
+    RESULTS = '\033[93m'
+    RESET = '\033[0m'
+
 def dice():
-        d_sides = input("What would you like to roll? ")
-        die = input("How many? ")
-        for _ in range(die):
-                print(r.randint(1, d_sides))
+    d_sides = input(color.MAIN + "What would you like to roll? " + color.RESET)
+    die = input(color.MAIN + "How many? " + color.RESET)
+    for _ in range(die):
+        print(color.RESULTS + str(r.randint(1, d_sides)) + color.RESET)
+
 while True:
         dice()
